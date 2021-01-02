2ab739a3b19b219f3e6ecae06f96bd93844f946c
Josh Klina <joshua.klina@gmail.com>
2021-01-02 13:31:48 -0700

    Setup projects and playbook

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+  include HighVoltage::StaticPage
+  layout :layout_for_page
+
+  private
+
+  def layout_for_page
+    case params[:id]
+    when /^playbook\D*/
+      "playbook"
+    else
+      "application"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to: "pages#show", id: "home"
+
+  get "/pages/*id" => "pages#show", as: :page, format: false
+
   resources :contact_submissions, only: [:new, :create]
 end
